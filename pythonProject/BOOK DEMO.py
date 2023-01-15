b1033a48a22f0301806f59e1c834f08f3a004fda
pritish <pritish0391@gmail.com>
2023-01-16 01:52:38 +0530

    first commit

---
+++ pythonProject/BOOK DEMO.py
@@ -0,0 +1,9 @@
+print('beatiful'  'weather'  'noida')
+
+print(''' good morning everyone .....
+what a beautiful day
+thank's''')
+
+a = '''What's your name'''
+input(a)
+print(a)
---
+++ pythonProject/helloworld.py
@@ -0,0 +1,32 @@
+import selenium as selenium
+
+print("helloworld")
+a = 12
+b = " hi "
+print ( b , a )
+
+# data type "LIST"
+
+
+a = [1, 2, 3, "RAJ", 7]
+print (a[0])
+
+# Data type TUPLE
+# In Tuple we cannot Modify the Variable Declared .
+# In tuple we use parenthesis while in list we use square brackets
+
+a = ( 1, 2, 3, 4)
+print(a)
+print(a[3])
+# tuple having multiple data types
+b =( "hello" , 1, 2, 3, "go")
+print(b)
+
+print(b[4])
+print(type(a))
+
+#dictionary Data type
+
+a = {1: "first name" ,2: "last name" ,3 : 33}
+#print value having key=1
+print(a[2])
